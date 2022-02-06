3862d0cfaedd5540d0ed4123cfb00e0a6e5b0397
Jaimit Patel <jaimit_12@hotmail.com>
2022-02-06 15:43:47 -0500

    fixed issue with .catch functions

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -9,11 +9,8 @@ router.get('/notes', (req, res) => {
     .getNote()
     .then(notes => 
     res.json(notes))
-      .catch ((err: any) => {
-        res.status(500).json({
-            error: err
-        })
-    })
+    .catch(err => res.status(500).json(err));
+
 });
 
 router.post('notes', (req, res) => {
@@ -21,25 +18,17 @@ router.post('notes', (req, res) => {
     .postNote(req.body)
     .then((note) => 
     res.json(note))
-    .catch ((err: any) => {
-        res.status(500).json({
-            error: err
-        })
-    })
+    .catch(err => res.status(500).json(err));
 });
 
 //BONUS Delete request;
 
-router.delete('notes/:id' (req, res) => {
+router.delete('notes/:id', (req, res) => {
     data
     .deleteNote(req.params.id)
     .then(() =>
      res.json( { ok:true }))
-        .catch ((err: any) => {
-        res.status(500).json({
-            error: err
-        })
-    })
+    .catch(err => res.status(500).json(err));
 });
 
 module.exports = router;
