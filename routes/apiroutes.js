20e38fddec2e52c7b6849d438e7abd2fe6244b18
Jaimit Patel <jaimit_12@hotmail.com>
2022-02-06 22:28:56 -0500

    fixed issue with delete route, gave each note a unique ID

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,41 +1,53 @@
 const router = require('express').Router();
 const notes = require("../db/db");
 const fs = require('fs');
-
-fs.readFile('db/db.json', 'utf8', (err, data) => {
-    if (err) throw err;
-const notes = JSON.parse(data)
-});
+const uuid = require('uuid')
+const path = require('path')
 
 router.get('/notes', (req, res) => {
-   res.json(notes)
-
-});
-
-router.post('/notes', (req, res) => {
-let newNote = req.body
-notes.push(newNote)
-updatedb()
-return console.log('Added a new note:' +newNote.title)
-});
-router.get('/notes/:id', (req, res) => {
-    res.json(notes[req.params.id])
+    fs.readFile(path.join(__dirname, '../db/db.json'), (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data)
+        res.json(notes);
+    })
 })
 
-//BONUS Delete request;
+router.post('/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, '../db/db.json'), (err, data) => {
+        if (err) throw err;
+        const notes = JSON.parse(data)
+        const newNote = req.body
+        newNote.id = uuid.v4()
+        notes.push(newNote)
+
+        const createNote = JSON.stringify(notes)
+        fs.writeFile(path.join(__dirname, '../db/db.json'), createNote, (err) => {
+            if (err) throw err
+        })
+        res.json(newNote)
+        return console.log(`Added a new note: ${newNote.id}`)
+
+    })
+})
 
 router.delete('/notes/:id', (req, res) => {
-    notes.splice(req.params.id, 1)
-   updatedb()
-   console.log('Deleted selected note!')
-});
+    const noteId= req.params.id
+    fs.readFile(path.join(__dirname, '../db/db.json'), (err, data) => {
+        if (err) throw err
+        const notes = JSON.parse(data)
+        notes.forEach(function(updatedNote, i) {
+            if (updatedNote.id.toString() === noteId) {
+                notes.splice (i, 1)
+            }
+        })
+            fs.writeFile('./db/db.json', JSON.stringify(notes), err => {
+                if (err) throw err;
+                console.log (`deleted selected note: ${noteId}`)
 
-function updatedb() {
-    fs.writeFile('db/db.json', JSON.stringify(notes), err =>
-    { if (err) throw err;
-    return true;
+            })
     })
-}
+    res.send('file')
+});
 
 module.exports = router;
 
